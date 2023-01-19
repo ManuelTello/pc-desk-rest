411de5f6ef1256705fdbd6b3511afe03ae322ef7
ManuelTello <93638568+ManuelTello@users.noreply.github.com>
2023-01-19 17:31:58 -0300

    Migraciones agregadas para la alta de nuevos campos y cambios en los constrains de la entidad Orders

---
+++ pc-desk-rest/Migrations/20230118185710_OrderModelChange.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pcdeskrest.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderModelChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
